b5b5caa90d7869e82b8f1d87efe3b086587960f7
leonardodalinky <493987054@qq.com>
2020-07-05 09:33:59 -0700

    update up to Lab 3

---
+++ myRcore/os/src/interrupt/context.rs
@@ -0,0 +1,8 @@
+use riscv::register::{sstatus::Sstatus, scause::Scause};
+
+#[repr(C)]
+pub struct Context {
+    pub x: [usize; 32],     // 32 个通用寄存器
+    pub sstatus: Sstatus,
+    pub sepc: usize
+}
---
+++ myRcore/os/src/interrupt/mod.rs
@@ -0,0 +1,16 @@
+//! 中断模块
+//! 
+//! 
+
+mod handler;
+mod context;
+mod timer;
+
+/// 初始化中断相关的子模块
+/// 
+/// - [`handler::init`]
+/// - [`timer::init`]
+pub fn init() {
+    handler::init();
+    println!("mod interrupt initialized");
+}
